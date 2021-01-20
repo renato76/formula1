63dc72656e2e142e13b9871c21f98af22783854f
renato76 <renmignogna@gmail.com>
2021-01-20 00:05:20 +0000

    created drivers model and hooked up to admin site - all working correctly

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Driver
 
-# Register your models here.
+admin.site.register(Driver)
--- drivers/models.py
+++ drivers/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 class Driver(models.Model):
-
-    name = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    driver_number = models.CharField(max_length=5)
-    team = models.CharField(max_length=25)
-    points = models.CharField(max_length=5)
+    driver_number = models.PositiveIntegerField(unique=True)
+    team = models.CharField(max_length=20)
+    points = models.PositiveIntegerField(max_length=5)
     image = models.CharField(max_length=100)
     flag = models.CharField(max_length=100)
+    country = models.CharField(max_length=30)
+    podiums = models.PositiveIntegerField(max_length=5)
+    points_to_date = models.PositiveIntegerField(max_length=5)
+    world_championships = models.PositiveIntegerField(max_length=3)
 
     def __str__(self):
-        return f'{self.name} {self.surname}'
+        return f'{self.firstname} {self.surname}'
