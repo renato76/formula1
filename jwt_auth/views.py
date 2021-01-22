bd9b7e5a47c0167ede157ec50bb8c0a8e20840f4
renato76 <renmignogna@gmail.com>
2021-01-22 15:33:42 +0000

    created login view - tested in Insomnia all working as expected

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,7 +1,11 @@
+from datetime import datetime, timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 from django.contrib.auth import get_user_model
+from django.conf import settings
+import jwt
 
 from .serializers.common import UserSerializer
 
@@ -17,4 +21,28 @@ class RegisterView(APIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(user_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY) 
+
+class LoginView(APIView):
+
+    def get_user(self, email):
+        try: 
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise PermissionDenied(detail='Invalid Credentials')
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user_to_login = self.get_user(email=email)
+        if not user_to_login.check_password(password):
+            raise PermissionDenied(detail='Invalid Credentials')
+        dt  = datetime.now() + timedelta(days=7)
+        token = jwt.encode(
+            { 'sub': user_to_login.id, 'exp': int(dt.strftime('%s'))},
+            settings.SECRET_KEY,
+            algorithm='HS256'
+        )
+        return Response(
+            {'token': token, 'message': f'Welcome Back {user_to_login.username}'}
+        )
 
