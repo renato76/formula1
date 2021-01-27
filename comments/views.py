3fb4db55c8a84c9d9c4d49b6b28c2af800757739
renato76 <renmignogna@gmail.com>
2021-01-27 22:15:22 +0000

    added comments Detail view and  delete a comment - tested in Insomnia - working

--- comments/views.py
+++ comments/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers.common     import CommentSerializer
-# from .models import Comment
+from .models import Comment
 
 
 class CommentListView(APIView):
@@ -15,3 +15,15 @@ class CommentListView(APIView):
             comment_to_create.save()
             return Response(comment_to_create.data, status=status.HTTP_201_CREATED)
         return Response(comment_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class CommentDetailView(APIView):
+    def get_comment(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise NotFound()
+
+    def delete(self, _request, pk):
+        comment_to_delete = self.get_comment(pk=pk)
+        comment_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
