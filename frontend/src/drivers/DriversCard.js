d703c79012a4aa718e0ec44b53416d6bde335e1d
renato76 <renmignogna@gmail.com>
2021-01-29 16:56:38 +0000

    saving changes to driverscard styling

--- frontend/src/drivers/DriversCard.js
+++ frontend/src/drivers/DriversCard.js
@@ -5,7 +5,7 @@ const DriversCard = (props) => {
   return (
     <div className="drivers-card-container">
       <div className="card-topbar">
-        <h3>topbar</h3>
+        <h3></h3>
       </div>
       <div className="card-namebar">
         <h2>{firstname} {surname}</h2>
