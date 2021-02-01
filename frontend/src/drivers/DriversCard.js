72ebf8c350bdb2627d5401010a4b0f2b13b16a33
renato76 <renmignogna@gmail.com>
2021-02-01 16:02:16 +0000

    styling added to drivercard

--- frontend/src/drivers/DriversCard.js
+++ frontend/src/drivers/DriversCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const DriversCard = (props) => {
-  const { title, firstname, surname, image, team, points } = props
+  const { title, firstname, surname, image, team, points, flag } = props
   return (
     <div className="drivers-card-container">
       <div className="card-topbar">
@@ -18,13 +18,20 @@ const DriversCard = (props) => {
           <h4>{firstname}</h4>
           <h3>{surname}</h3>
         </div>
+        <div className="card-flag">
+          <figure className="flag-container">
+            <img className="flag" src={flag} alt="flag" />
+          </figure>
+        </div>
       </div>
       <div className="card-constructor">
         <h4>{team}</h4>
       </div>
-      <div className="image-background">
-        <div className="card-image">      
-          <img src={image} alt={title} />   
+      <div className="grid-container">
+        <div className="image-background">
+          <div className="card-image">      
+            <img src={image} alt={title} />   
+          </div>
         </div>
       </div>
     </div>
