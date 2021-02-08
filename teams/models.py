31163c730210dab4d489c8ac7ae3585c7d576732
renato76 <renmignogna@gmail.com>
2021-02-08 19:39:18 +0000

    added all the  Team fields in backend

--- teams/models.py
+++ teams/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-# from drivers.models import Driver
 
 class Team(models.Model):
     name = models.CharField(max_length=50)
+    full_team_name = models.CharField(max_length=50)
     team_logo = models.CharField(max_length=300)
     car_image = models.CharField(max_length=300)
+    base = models.CharField(max_length=300)
+    team_chief = models.CharField(max_length=300)
+    technical_chief = models.CharField(max_length=300)
+    chassis = models.CharField(max_length=300)
+    power_unit = models.CharField(max_length=300)
+    first_team_entry = models.PositiveIntegerField() 
+    world_championships = models.PositiveIntegerField() 
+    pole_positions = models.PositiveIntegerField() 
+    fastest_laps = models.PositiveIntegerField() 
+    team_profile = models.CharField(max_length=3000)
 
     def __str__(self):
         return f'{self.id} - {self.name}'
